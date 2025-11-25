a85b607550430f73803964089a2b8cbd62f68abe
shivatyagi01 <shivaworx1@gmail.com>
2025-11-25 21:12:22 +0530

    added owl-carousel, hero section skeleton

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <div className='container'>
+                <div className="row">
+                    <div className="col-3">
+
+                    </div>
+                    <div className="col-6"></div>
+                    <div className="col-3"></div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+    return (
+        <div className='hero-section'>
+            <div className="container">
+                <div className="hero-inner">
+                    <h1>Explore Gadgets: Unlock Innovation!</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid placeat porro accusamus, eum modi eveniet! Impedit incidunt soluta eos cumque, facilis odio? Nobis maxime dignissimos inventore non sapiente nostrum culpa.</p>
+                    <a href="#" className="primary-btn">Get In Touch</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
